fe8509fd9090d5006be339b5d7795909e46ad9a8
hamza043 <muhamadhamza474@gmail.com>
2023-10-30 23:21:05 +0500

    Array Methods

---
+++ app.js
@@ -0,0 +1,46 @@
+// const head = document.querySelector(".head");
+
+
+// function headChange(){
+// head.innerHTML = "change to javascript";
+
+// }
+
+// function CalculateSum(){
+//     let num1= Number(prompt("Enter First Number"));
+//     let num2= Number(prompt("Enter Second Number"));
+
+//     if (num1 && num2) {
+        
+//         var sum = num1 + num2;
+//     }
+//     document.querySelector('#result').innerHTML = 'First value = ' + num1 + '<br>Second value = ' + num2 + '<br>result = ' + sum;
+
+// }
+
+// let fruit = ['mango' , 'orange' , 'watermelon' , 'banana']
+// console.log(fruit[1]);
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// num.splice(6, 3, 17, 18, 19);
+// console.log(num);
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// num.pop();
+// num.splice(4, 1,);
+// num.shift();
+// console.log(num);
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let NewNum = num.slice(2,7);
+// console.log(NewNum);
+
+// let names = "Muhammad Hamza Sheikh";
+// let NewNames = names.split(' ').join(' ');
+// console.log(NewNames);
+
+let arr = ['mango',' ' + 'banana', ' ' + 'orange',' ' + 'apple'];
+
+console.log('All fruits names are ' + arr);
